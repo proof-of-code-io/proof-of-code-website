11e5e9fda3d92869450d864cdb43e96d0047edb0
Matthieu Auger <matthieua@theodo.fr>
2022-03-21 12:11:20 +0100

    Proof of Code

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,18 @@
 module.exports = {
   plugins: [
     {
-      resolve: "@matthieuauger/gatsby-theme-meetup"
+      resolve: "@matthieuauger/gatsby-theme-meetup",
+      options: {
+        meetupName: "Proof of Code",
+        meetupHomepageHeadline: "The Web3 meetup for developers 🍵",
+        meetupDotComGroupUrlName: "proof-of-code",
+        displayVideosLink: false,
+        //meetupVideosUrl = 'https://www.youtube.com/channel/UC66eQOycjMnaqzpbRUhEK2w'
+        talkProposalUrl: "#",
+        //textBlocksPath: `${__dirname}/src/text-blocks`,
+        currentMeetupColor: "#EFCC74",
+        pastMeetupColors: ["#E6BB91", "#DDDEC4", "#F3DBD1"]
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
